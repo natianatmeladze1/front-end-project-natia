10030cb58b83b32767fc025b2fd125ee742d364b
natianatmeladze1 <natia.natmeladze.1@btu.edu.ge>
2026-01-19 17:25:22 +0400

    form validation, scroll on top

--- js/script.js
+++ js/script.js
@@ -2,15 +2,39 @@
 let burger = document.querySelector('.burger');
 let ulElements = document.querySelector('.ul-elements');
 
-
 burger.addEventListener('click', () => {
     ulElements.classList.toggle('active');
 });
 
 
-let menuLinks = document.querySelectorAll('.ul-elements li a');
-menuLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        ulElements.classList.remove('active');
+// form validation
+const registerForm = document.querySelector('.register-form');
+const inputs = document.querySelectorAll('.register-input');
+
+registerForm.addEventListener('submit', (event) => {
+    let hasError = false;
+
+    inputs.forEach(input => {
+        if (input.value.trim() === "") {
+            input.style.border = "2px solid red";
+            input.placeholder = "Fill the field!";
+            input.value = ""; 
+            hasError = true;
+        } else {
+            input.style.border = "1px solid #c9dff3";
+        }
     });
+    if (hasError) {
+        event.preventDefault();
+    } else {
+        console.log("Form submitted successfully!");
+    }
 });
+
+
+//scroll on top
+const btn = document.getElementById("topBtn");
+window.onscroll = () => btn.style.display = window.scrollY > 300 ? "block" : "none";
+btn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+
+
