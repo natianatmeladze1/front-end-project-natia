c4c2566f91bcfcc86fe580dabf9fc7c30a4656e9
natianatmeladze1 <natia.natmeladze.1@btu.edu.ge>
2026-01-19 18:21:59 +0400

    final version

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ burger.addEventListener('click', () => {
 });
 
 
+
+
+
 // form validation
 const registerForm = document.querySelector('.register-form');
 const inputs = document.querySelectorAll('.register-input');
@@ -32,9 +35,59 @@ registerForm.addEventListener('submit', (event) => {
 });
 
 
+
+
+
+
 //scroll on top
 const btn = document.getElementById("topBtn");
 window.onscroll = () => btn.style.display = window.scrollY > 300 ? "block" : "none";
 btn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
 
 
+
+
+
+
+//axios
+const hotelContainer = document.getElementById('hotels-container');
+const hotelPhotos = [
+    "https://images.unsplash.com/photo-1631049307264-da0ec9d70304?q=80&w=400&auto=format&fit=crop",
+    "https://images.unsplash.com/photo-1571896349842-33c89424de2d?q=80&w=400&auto=format&fit=crop",
+    "https://images.unsplash.com/photo-1540518614846-7eded433c457?q=80&w=400&auto=format&fit=crop"
+];
+
+axios.get('https://jsonplaceholder.typicode.com/posts?_limit=3')
+    .then(res => {
+        const posts = res.data;
+        hotelContainer.innerHTML = ''; 
+
+        posts.forEach((post, index) => {
+            hotelContainer.innerHTML += `
+                <div class="div3">
+                    <img src="${hotelPhotos[index]}" alt="Hotel Room">
+                    <h6 class="h6">Travelsy Room ${index + 1}</h6>
+                    <p class="p-el">${post.title.slice(0, 40)}...</p>
+                    <h6 class="price">${100 + (index * 50)}$ /per night</h6>
+                    <div class="i-el"><i class="fa-solid fa-circle-chevron-down"></i></div>
+                </div>`;
+        });
+    })
+    .catch(err => console.error("API Error:", err));
+
+
+
+
+
+    
+//localstorage
+const cookieBox = document.getElementById('cookieBox');
+const acceptBtn = document.getElementById('acceptBtn');
+
+if (!localStorage.getItem('cookieAccepted')) {
+    cookieBox.style.display = "flex"; 
+}
+acceptBtn.addEventListener('click', () => {
+    cookieBox.style.display = "none";
+    localStorage.setItem('cookieAccepted', 'true');
+});
